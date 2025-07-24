dad4c5d7f85f6bba6c6687c65263ca665d61a46c
Wdson Danrlei <44691537+wdson91@users.noreply.github.com>
2025-07-24 17:03:22 +0100

    Update sftp_connection.py

--- sftp_connection.py
+++ sftp_connection.py
@@ -39,7 +39,7 @@ def download_files_from_sftp():
     
     try:
         # Pasta remota onde estão as pastas por NIF
-        pasta_remota = 'uploads'
+        pasta_remota = 'home/mydreami/myDream'
         
         # Pasta local onde os arquivos serão salvos
         pasta_local = './downloads'
